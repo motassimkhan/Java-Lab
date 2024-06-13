5ca7c0b36cffe6d4e1cbd01b10ab57428623008b
Motassim Khan <motassimkhan4@gmail.com>
2024-06-13 17:47:59 +0530

    2D array

---
+++ Arrays/MultiD_Array.java
@@ -0,0 +1,14 @@
+class MultiD_Array {
+    public static void main(String [] args)
+    {
+        int array[][] = {{1,2,3},{4,5,6},{7,1,2}};
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array.length; j++)
+            {
+                System.out.println(array[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
