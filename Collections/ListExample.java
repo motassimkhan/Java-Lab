582081e5d3a1455af74d4baaab49b0f04ebe81b5
Motassim Khan <motassimkhan4@gmail.com>
2024-06-29 07:11:23 +0530

    Collections

---
+++ Collections/ListExample.java
@@ -0,0 +1,13 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+    }
+}
