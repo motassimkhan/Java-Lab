c641ce4ffee8a404ce0971532683413e540f75da
AymanUzayr <68193240+AymanUzayr@users.noreply.github.com>
2024-07-16 06:16:31 +0530

    Update MapExample.java

--- Collections/MapExample.java
+++ Collections/MapExample.java
@@ -1,6 +1,16 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapExample {
     public static void main(String[] args) {
-        
+        Map<Integer, String> m = new HashMap<>();
+        m.put(1, "Apple");
+        m.put(2, "Banana");
+        m.put(3, "Mango");
+
+        System.out.println("Map elements:");
+        for (var entry : m.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
